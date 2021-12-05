0006a0b2a3f6aed396af8ccae5f89e15effae0cd
Seungju Kim <seungjukim0522@gmail.com$>
2021-12-05 19:51:35 +0900

    last

--- Buzzer/buzzertest.c
+++ Buzzer/buzzertest.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <dirent.h>
 #define MAX_SCALE_STEP 8
-#define BUZZER_BASE_SYS_PATH "/sys/bus/platform/devices/”
 #define BUZZER_FILENAME "peribuzzer"
 #define BUZZER_ENABLE_NAME "enable"
 #define BUZZER_FREQUENCY_NAME "frequency"
