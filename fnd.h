65e1f8bdf482500badb4418df279ec01becfb777
JunwooHyun <seh6512@daum.net>
2021-12-07 01:27:07 +0900

    초안
    수정요함

---
+++ fnd.h
@@ -0,0 +1,34 @@
+
+#ifndef __FND_DRV_H__
+#define __FND_DRV_H__
+
+#define MAX_FND_NUM			6
+#define FND_DATA_BUFF_LEN	(MAX_FND_NUM + 2)
+
+
+typedef struct FNDWriteDataForm_tag 
+{
+	char	DataNumeric[FND_DATA_BUFF_LEN];
+	char	DataDot[FND_DATA_BUFF_LEN];
+	char	DataValid[FND_DATA_BUFF_LEN];	
+}stFndWriteForm,*pStFndWriteForm;
+
+
+void doHelp(void);
+int fndDisp(int num , int dotflag);
+int fndOff();
+int fndtime();
+
+
+#endif// __FND_DRV_H__
+
+
+
+
+
+
+
+
+
+
+
