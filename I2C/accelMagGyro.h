655001b50ed57020e31e82ff0192677cdc3e5d45
Seungju Kim <seungjukim0522@gmail.com$>
2021-12-11 21:10:01 +0900

    I2C_accelMagGyro

---
+++ I2C/accelMagGyro.h
@@ -0,0 +1,8 @@
+#ifndef _ACCELMAGGYRO_H_
+#define _ACCELMAGGYRO_H_
+
+int Accelerometer(void);
+int Magnetometer(void);
+int Gyroscope(void);
+
+#endif
---
+++ I2C/accelMagGyroTest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "accelMagGyro.h"
+
+#define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/"
+#define MAGNEPATH "/sys/class/misc/FreescaleMagnetometer/"
+#define GYROPATH "/sys/class/misc/FreescaleGyroscope/"
+
+int main(void){
+
+    Accelerometer(); // 가속도계
+    Magnetometer(); // 자기장계
+    Gyroscope(); // 기울기계
+
+}
